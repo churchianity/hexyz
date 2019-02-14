2291bcad45fabf105a3576317e53d83598ab9f96
churchianity <hayashi.nicholas@gmail.com>
2019-02-13 23:57:16 -0500

    cleaned up a bunch of library functions.

--- hex.lua
+++ hex.lua
@@ -1,31 +1,14 @@
------ [[ AXIAL/CUBE COORDINATE SYSTEM FOR AMULET/LUA]] -------------------------
+----- [[ AXIAL/CUBE COORDINATE HEXAGON LIBRARY FOR AMULET/LUA]] ----------------
 --[[                                                     author@churchianity.ca
         -- INTRODUCTION
     this is a hexagonal grid library for amulet/lua.
     it uses axial coordinates or cube/hex coordinates when necessary.
     by amulet convention, hexes are either vec2(s, t) or vec3(s, t, z)
     but nearly always the former. 
-    
-    in some rare cases, coordinates will be passed individually, usually
-    because they are only passed internally and should never be adjusted
-    directly.
 
-    in amulet, vector arithmetic already works via: + - * / 
-    additional things such as equality, and distance are implemented here.
-    
-    +support for parallelogram, triangular, hexagonal and rectangular maps.
-    +support for arbitrary maps with gaps via hashmaps-like storage
-    +support for simple irregular hexagons (horizontal and vertical stretching).
-
-    classes are used sparsely. maps implement a few constructors for storing
-    your maps elsewhere, and should be the only field that is necessarily 
-    visible outside the library.
-
-        -- RESOURCES USED TO DEVELOP THIS LIBRARY
+        -- RESOURCES USED TO DEVELOP THIS LIBRARY, AND FOR WHICH I AM GRATEFUL
     https://redblobgames.com/grid/hexagons    - simply amazing. 
     http://amulet.xyz/doc                     - amulet documentation
-    TODO that place that had the inner circle/outer circle ratio?? 
-
   ]]
 
 ----- [[ GENERALLY USEFUL FUNCTIONS ]] -----------------------------------------
@@ -35,7 +18,7 @@ local function round(n)
     return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
 end
 
------ [[ HEX CONSTANTS ]] ------------------------------------------------------
+----- [[ HEX CONSTANTS & UTILITY FUNCTIONS ]] ----------------------------------
 
 -- all possible vector directions from a given hex by edge
 local HEX_DIRECTIONS = {vec2( 1 ,  0), 
@@ -45,20 +28,19 @@ local HEX_DIRECTIONS = {vec2( 1 ,  0),
                         vec2(-1 ,  1), 
                         vec2( 0 ,  1)}
 
------ [[ HEX UTILITY FUNCTIONS ]] ----------------------------------------------
-
-function hex_equals(a, b)
-    return a.s == b.s and a.t == b.t
+-- return hex vector direction via index |direction|.
+function hex_direction(direction)
+    return HEX_DIRECTIONS[direction]
 end
 
-function hex_length(hex)
-    return round(math.abs(hex.s) + math.abs(hex.r) + math.abs(-hex.s - hex.t)/2)
+-- return hexagon adjacent to |hex| in |direction|
+function hex_neighbour(hex, direction)
+    return hex + HEX_DIRECTION[direction]
 end
 
-function hex_distance(a, b)
-    return hex_length(a - b)
-end
-
+-- rounds hexes. without this, pixel_to_hex returns fractional coordinates.
+-- using single coordinates instead of a vector, because this should only
+-- ever be called internally.
 function hex_round(s, t)
     local rs = round(s)
     local rt = round(t)
@@ -82,15 +64,16 @@ end
 ----- [[ LAYOUT, ORIENTATION & COORDINATE CONVERSION  ]] -----------------------
 
 -- forward & inverse matrices used for the flat orientation.
-local FLAT = {3.0/2.0,  0.0,  3.0^0.5/2.0,  3.0^0.5, 
-              2.0/3.0,  0.0,  -1.0/3.0   ,  3.0^0.5/3.0}
+local FLAT = {M = mat2(3.0/2.0,  0.0,  3.0^0.5/2.0,  3.0^0.5    ),
+              W = mat2(2.0/3.0,  0.0,  -1.0/3.0   ,  3.0^0.5/3.0)}
 
--- forward & inverse matrices used for the pointy orientation. 
-local POINTY = {3.0^0.5,  3.0^0.5/2.0,  0.0,  3.0/2.0,  
-                3.0^0.5/3.0,  -1.0/3.0,  0.0,  2.0/3.0}
+-- forward & inverse matrices used for the pointy orientation.
+local POINTY = {M = mat2(3.0^0.5,  3.0^0.5/2.0,  0.0,   3.0/2.0),
+                W = mat2(3.0^0.5/3.0,  -1.0/3.0,  0.0,  2.0/3.0)}
 
+-- TODO encapsulate hex_to_pixel and pixel_to_hex in layout table.
 -- stores layout information that does not pertain to map shape 
-function layout_init(origin, size, orientation)
+function hex_layout(origin, size, orientation)
     return {origin      = origin      or vec2(0),
             size        = size        or vec2(11),
             orientation = orientation or FLAT}
@@ -98,117 +81,66 @@ end
 
 -- hex to screen
 function hex_to_pixel(hex, layout)
-    local M = layout.orientation
+    local M = layout.orientation.M
     
-    local x = (M[1] * hex.s + M[2] * hex.t) * layout.size.x
-    local y = (M[3] * hex.s + M[4] * hex.t) * layout.size.y
+    local x = (M[1][1] * hex.s + M[1][2] * hex.t) * layout.size.x
+    local y = (M[2][1] * hex.s + M[2][2] * hex.t) * layout.size.y
 
     return vec2(x + layout.origin.x, y + layout.origin.y)
 end
 
 -- screen to hex
 function pixel_to_hex(pix, layout)
-    local M = layout.orientation
+    local W = layout.orientation.W
 
     local pix = (pix - layout.origin) / layout.size 
 
-    local s = M[5] * pix.x + M[6] * pix.y
-    local t = M[7] * pix.x + M[8] * pix.y
+    local s = W[1][1] * pix.x + W[1][2] * pix.y
+    local t = W[2][1] * pix.x + W[2][2] * pix.y
 
     return hex_round(s, t) 
 end
 
 ----- [[ MAP STORAGE & RETRIEVAL ]] --------------------------------------------
-
---[[ _init functions return a table of tables;
-     a map of points in a chosen shape and specified layout.
-     
-     grammap_init       -       parallelogram map
-     trimap_init        -       triangular map
-     hexmap_init        -       hexagonal map
-     rectmap_init       -       rectangular map
-
-     calling .retrieve(pix) on your map will get the hexagon at that pixel.
-     calling .store(hex) on your map will store that hex as pixel coords.
-     
-     maps store coordinates like this:
-
-     map[hex] = hex_to_pixel(hex)
-
-     this means you should be able to get all the information you need about
-     various coordinates completely within the map 'class', without calling
-     any internal functions. indeed, *map_init, map.retrieve, and map.store
-     is all you need.
+--[[
   ]]
+-- TODO make all functions work regardless of layout.
 
--- returns parallelogram-shaped map. 
-function grammap_init(layout, width, height)
+-- returns unordered parallelogram-shaped map of |width| and |height|. 
+function hex_parallelogram_map(width, height)
     local map = {}
-    local mt = {__index={layout=layout, 
-
-            -- get hex in map from pixel coordinate
-            retrieve=function(pix)
-                return pixel_to_hex(pix, layout)
-            end,
-
-            -- store pixel in map from hex coordinate
-            store=function(hex)
-                map[hex]=hex_to_pixel(hex, layout)             
-            end
-            }}
+    local mt = {__index={width=width, height=height}} 
     
     setmetatable(map, mt)
     
     for s = 0, width do
         for t = 0, height do
-            table.insert(map, hex_to_pixel(vec2(s, t), layout)) 
+            map[vec2(s, t)] = true
         end
     end
     return map
 end
 
--- returns triangular map. 
-function trimap_init(layout, size)
+-- returns unordered triangular map of |size|.
+function hex_triangular_map(size)
     local map = {}
-    local mt = {__index={layout=layout,
-    
-            -- get hex in map from pixel coordinate
-            retrieve=function(pix)
-                return pixel_to_hex(pix, layout)
-            end,
-
-            -- store pixel in map from hex coordinate
-            store=function(hex)
-                map[hex]=hex_to_pixel(hex, layout)
-            end
-            }}
+    local mt = {__index={size=size}}
     
     setmetatable(map, mt)
 
     for s = 0, size do
         for t = size - s, size do
-            map.store(vec2(s, t))
+            map[vec2(s, t)] = true
         end
     end
     return map
 end
 
--- returns hexagonal map. length of map is radius * 2 + 1 
-function hexmap_init(layout, radius) 
+-- returns unordered hexagonal map of |radius|.  
+function hex_hexagonal_map(radius) 
     local map = {}
-    local mt = {__index={layout=layout,
+    local mt = {__index={radius=radius}}
             
-            -- get hex in map from pixel coordinate
-            retrieve=function(pix)
-                return pixel_to_hex(pix, layout)
-            end,
-
-            -- store pixel in map from hex coordinate
-            store=function(hex)
-                map[hex]=hex_to_pixel(hex, layout)        
-            end
-            }}
-
     setmetatable(map, mt)
 
     for s = -radius, radius do
@@ -216,33 +148,22 @@ function hexmap_init(layout, radius)
         local t2 = math.min(radius, -s + radius)
 
         for t = t1, t2 do
-            table.insert(map, hex_to_pixel(vec2(s, t), layout))
+            map[vec2(s, t)] = true
         end
     end
     return map
 end
 
--- returns rectangular map. 
-function rectmap_init(layout, width, height)
+-- returns unordered rectangular map of |width| and |height|. 
+function hex_rectangular_map(width, height)
     local map = {}
-    local mt = {__index={layout=layout,
+    local mt = {__index={width=width, height=height}}
                
-            -- get hex in map from pixel coordinate                    
-            retrieve=function(pix)
-                return pixel_to_hex(pix, layout)
-            end,
-
-            -- store pixel in map from hex coordinate
-            store=function(hex)
-                map[hex]=hex_to_pixel(hex - vec2(0, math.floor(hex.s/2)), layout) 
-            end
-            }}
-    
     setmetatable(map, mt) 
         
     for s = 0, width do
         for t = 0, height do
-            map.store(vec2(s, t))
+            map[vec2(s, t)] = true
         end
     end
     return map 
